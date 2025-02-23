6f3a2cd059921abc8827dfbcd737bd04d6e1db84
lilalin808 <lilalanlin@icloud.com>
2025-02-23 15:46:25 -0800

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,20 +1,27 @@
 // Initialize Firebase before calling any Firebase services
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, orderBy, query } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, orderBy, query } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
 
+ 
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBmS3PF33c4BHzgjKuM0LUSu_wpIFQSNvk",
+    authDomain: "peer-tutor-a1076.firebaseapp.com",
+    projectId: "peer-tutor-a1076",
+    storageBucket: "peer-tutor-a1076.firebasestorage.app",
+    messagingSenderId: "677806357185",
+    appId: "1:677806357185:web:c74d94070bf86997517240"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 // Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBmS3PF33c4BHzgjKuM0LUSu_wpIFQSNvk",
-  authDomain: "peer-tutor-a1076.firebaseapp.com",
-  projectId: "peer-tutor-a1076",
-  storageBucket: "peer-tutor-a1076.firebasestorage.app",
-  messagingSenderId: "677806357185",
-  appId: "1:677806357185:web:be5149be7ba68343517240"
-};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const db = getFirestore(app); // Firestore instance
 const auth = getAuth(); // Auth instance
 
