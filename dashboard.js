ca0dfe57919b142d75dd5dadd1954aec85c38902
lilalin808 <lilalanlin@icloud.com>
2025-02-28 16:21:27 -0800

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -54,7 +54,7 @@ async function loadQuestions() {
       };
 
       const repliesList = document.createElement("div");
-      repliesList.id = `repliesList-${questiondId}`;
+      repliesList.id = `repliesList-`+questiondId;
 
       const replyButton = document.createElement("button");
       replyButton.textContent = "Reply";
